8ee3992d0df6e2b6b3cee40e852820b5e39e7787
Bill Moritz <wmoritz@salesforce.com>
2016-04-22 00:06:53 -0400

    Rename package scripts Add packages Update README

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # dotfiles
+
 To install run the following code snip. Use at your own risk!
-```
+
+## installation
+
+``` sh
 git clone https://github.com/billmoritz/dotfiles.git
 cd dotfiles
 bin/dotfiles.sh
 ```
 
-Portions borrowed/edited from https://github.com/mathiasbynens/dotfiles.
+## packages
+
+``` sh
+dotfiles_pkg.sh
+```
+
+Portions borrowed/edited from <https://github.com/mathiasbynens/dotfile>
--- bin/dotfiles_pkg.sh
+++ bin/dotfiles_pkg.sh
@@ -8,7 +8,7 @@ brew update && brew upgrade
 
 # Tools
 brew cask install vagrant vagrant-manager
-brew install wget ncdu htop ncdu jq
+brew install wget ncdu htop ncdu jq spellcheck
 brew install --with-stoken --with-oath-toolkit openconnect
 
 # Docker
@@ -19,3 +19,6 @@ docker-machine create --driver virtualbox default
 # Services
 brew install rabbitmq memcached mysql
 ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
+
+# Pip
+pip install pylama
